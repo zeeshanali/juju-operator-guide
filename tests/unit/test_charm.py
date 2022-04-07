e3a28ef2df24b9eac6745fcf83b321467040ba6b
Zeeshan Ali Shah <javaclinic@gmail.com>
2022-04-07 16:15:41 +0500

    Update test_charm.py

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -20,48 +20,5 @@ class TestCharm(unittest.TestCase):
 
     def test_config_changed(self):
         self.assertEqual(list(self.harness.charm._stored.things), [])
-        self.harness.update_config({"thing": "foo"})
-        self.assertEqual(list(self.harness.charm._stored.things), ["foo"])
-
-    def test_action(self):
-        # the harness doesn't (yet!) help much with actions themselves
-        action_event = Mock(params={"fail": ""})
-        self.harness.charm._on_fortune_action(action_event)
-
-        self.assertTrue(action_event.set_results.called)
-
-    def test_action_fail(self):
-        action_event = Mock(params={"fail": "fail this"})
-        self.harness.charm._on_fortune_action(action_event)
-
-        self.assertEqual(action_event.fail.call_args, [("fail this",)])
-
-    def test_webserver_pebble_ready(self):
-        # Check the initial Pebble plan is empty
-        initial_plan = self.harness.get_container_pebble_plan("webserver")
-        self.assertEqual(initial_plan.to_yaml(), "{}\n")
-        # Expected plan after Pebble ready with default config
-        expected_plan = {
-            "services": {
-                "webserver": {
-                    "override": "replace",
-                    "summary": "webserver",
-                    "command": "gunicorn -b 0.0.0.0:80 webserver:app -k gevent",
-                    "startup": "enabled",
-                    "environment": {"thing": "🎁"},
-                }
-            },
-        }
-        # Get the webserver container from the model
-        container = self.harness.model.unit.get_container("webserver")
-        # Emit the PebbleReadyEvent carrying the webserver container
-        self.harness.charm.on.webserver_pebble_ready.emit(container)
-        # Get the plan now we've run PebbleReady
-        updated_plan = self.harness.get_container_pebble_plan("webserver").to_dict()
-        # Check we've got the plan we expected
-        self.assertEqual(expected_plan, updated_plan)
-        # Check the service was started
-        service = self.harness.model.unit.get_container("webserver").get_service("webserver")
-        self.assertTrue(service.is_running())
-        # Ensure we set an ActiveStatus with no message
-        self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+        self.harness.update_config({"thing": "zeeshan"})
+        self.assertEqual(list(self.harness.charm._stored.things), ["zeeshan"])
