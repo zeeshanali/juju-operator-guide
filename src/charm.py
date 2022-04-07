0faf5557b019082f20917b3b78a8c65fd1aedc4f
Zeeshan Ali Shah <javaclinic@gmail.com>
2022-04-07 18:07:01 +0500

    Update charm.py

--- src/charm.py
+++ src/charm.py
@@ -13,11 +13,12 @@ develop a new k8s charm using the Operator Framework:
 """
 
 import logging
+import urllib
 
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, MaintenanceStatus
 
 logger = logging.getLogger(__name__)
 
@@ -30,9 +31,26 @@ class OperatorTemplateCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
         self.framework.observe(self.on.webserver_pebble_ready, self._on_webserver_pebble_ready)
+        self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(self.on.fortune_action, self._on_fortune_action)
         self._stored.set_default(things=[])
+
+    def _on_install(self, _):
+        # Download the site
+        self._fetch_site()
+
+    def _fetch_site(self):
+        """Fetch latest copy of website from Github and move into webroot"""
+        # Set the site URL
+        site_src = "https://zeeshanali.com/taskob"
+        # Set some status and do some logging
+        self.unit.status = MaintenanceStatus("Fetching web site")
+        logger.info("Downloading site from %s", site_src)
+        # Download the site
+        urllib.request.urlretrieve(site_src, "/data/index.html")
+        # Set the unit status back to Active
+        self.unit.status = ActiveStatus()
 
     def _on_webserver_pebble_ready(self, event):
         """Define and start a workload using the Pebble API.
